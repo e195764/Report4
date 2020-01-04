1c276157e9a84151bf3519a159e9cfb6d2729f52
Wataru Kinjo <e195764@ie.u-ryukyu.ac.jp>
2020-01-04 12:22:40 +0900

    ステップ2: 例外処理を記述せよ。

--- src/jp/ac/uryukyu/ie/e195764/Exception.java
+++ src/jp/ac/uryukyu/ie/e195764/Exception.java
@@ -3,6 +3,11 @@ package jp.ac.uryukyu.ie.e195764;
 public class Exception {
     public static void main(String[] args) {
         String str = null;
-        System.out.println(str.length());
+        try {
+            System.out.println(str.length());
+        } catch (NullPointerException e) {
+            System.out.println("例外が発生しました");
+            System.out.println(e.getMessage());
+        }
     }
 }
