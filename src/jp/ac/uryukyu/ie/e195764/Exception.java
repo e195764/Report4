4eea1ad54d999e722fce0de7d1194cb6df2d73ba
Wataru Kinjo <e195764@ie.u-ryukyu.ac.jp>
2020-01-04 12:30:36 +0900

    ステップ3: APIリファレンスを参照してみよう。

--- src/jp/ac/uryukyu/ie/e195764/Exception.java
+++ src/jp/ac/uryukyu/ie/e195764/Exception.java
@@ -2,12 +2,7 @@ package jp.ac.uryukyu.ie.e195764;
 
 public class Exception {
     public static void main(String[] args) {
-        String str = null;
-        try {
-            System.out.println(str.length());
-        } catch (NullPointerException e) {
-            System.out.println("例外が発生しました");
-            System.out.println(e.getMessage());
-        }
+        String str = "百二十三";
+        int value =  Integer.parseInt(str);
     }
 }
